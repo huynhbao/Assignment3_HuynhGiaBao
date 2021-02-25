62bca0ef19dd441c5e294d195bda75eab12c9820
HuynhBao <huynhbao162000@gmail.com>
2021-02-25 20:22:29 +0700

    Update1

--- src/java/daos/UserDAO.java
+++ src/java/daos/UserDAO.java
@@ -5,10 +5,56 @@
  */
 package daos;
 
+import dtos.UserDTO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.apache.log4j.Logger;
+import utils.DBUtils;
+
 /**
  *
  * @author HuynhBao
  */
 public class UserDAO {
-    
+    private static final Logger LOGGER = Logger.getLogger(UserDAO.class);
+
+    public UserDTO checkLogin(String email, String password) throws SQLException {
+        UserDTO result = null;
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.getConnection();
+            if (conn != null) {
+                String sql = "SELECT name, email, status, roleID FROM tblUsers WHERE email = ? AND password = ?";
+                stm = conn.prepareStatement(sql);
+                stm.setString(1, email);
+                stm.setString(2, password);
+                rs = stm.executeQuery();
+
+                if (rs.next()) {
+                    String name = rs.getString("name");
+                    String status = rs.getString("status");
+                    String roleID = rs.getString("roleID");
+                    result = new UserDTO(email, name, "", status, roleID);
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.error(e.toString());
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return result;
+    }
+
 }
